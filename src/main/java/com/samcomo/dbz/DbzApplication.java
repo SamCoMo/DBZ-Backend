2a65b09c2e2e06a638825f05fce77527db687b05
jujaeyeong <jjy5230@gmail.com>
2024-02-26 02:27:19 +0900

    first commit

---
+++ src/main/java/com/samcomo/dbz/DbzApplication.java
@@ -0,0 +1,13 @@
+package com.samcomo.dbz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samcomo/dbz/DbzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samcomo.dbz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
