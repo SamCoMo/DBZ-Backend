4bdb376c43a5dd7f3f375f191e470374f12b8b0f
JGoo99 <zzgg3040@naver.com>
2024-03-03 04:44:53 +0900

    feature: 모델 구현
    - 회원가입 dto (request, response)
    - 이메일, 닉네임 중복 에러 enum
    - 소셜 회원가입 방식 enum

--- src/main/java/com/samcomo/dbz/global/exception/ErrorCode.java
+++ src/main/java/com/samcomo/dbz/global/exception/ErrorCode.java
@@ -9,11 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
   // Global
-
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 문제가 발생했습니다."),
-
   INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-
   ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
 
   // AWS S3
@@ -24,6 +21,8 @@ public enum ErrorCode {
 
   // Member
   MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 정보를 찾을 수 없습니다."),
+  EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일 계정입니다."),
+  NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임입니다."),
 
   // Report
   REPORT_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글 정보를 찾을 수 없습니다."),
---
+++ src/main/java/com/samcomo/dbz/member/model/dto/RegisterDto.java
@@ -0,0 +1,80 @@
+package com.samcomo.dbz.member.model.dto;
+
+import static com.samcomo.dbz.member.model.constants.MemberRole.MEMBER;
+import static com.samcomo.dbz.member.model.constants.MemberStatus.ACTIVE;
+
+import com.samcomo.dbz.member.model.constants.MemberRole;
+import com.samcomo.dbz.member.model.constants.MemberStatus;
+import com.samcomo.dbz.member.model.entity.Member;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+public class RegisterDto {
+
+  @Getter
+  @Builder
+  public static class Request {
+
+    private String email;
+    private String nickname;
+    private String phone;
+    private String password;
+
+    public static Member toEntity(Request request) {
+      return Member.builder()
+          .email(request.getEmail())
+          .nickname(request.getNickname())
+          .profileImageUrl("defaultImageUrl.jpg") // TODO 기본 이미지 url 정하기
+          .phone(request.getPhone())
+          .role(MEMBER)
+          .status(ACTIVE)
+          .build();
+    }
+  }
+
+  @Getter
+  @Builder
+  @ToString
+  public static class Response {
+
+    private Long memberId;
+    private MemberInfo memberInfo;
+    private MemberStatus status;
+    private MemberRole role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Getter
+    @Builder
+    @ToString
+    public static class MemberInfo {
+
+      private String email;
+      private String nickname;
+      private String profileImageUrl;
+      private String phone;
+    }
+
+    public static Response fromEntity(Member member) {
+
+      return Response.builder()
+          .memberId(member.getId())
+          .memberInfo(
+              MemberInfo.builder()
+                  .email(member.getEmail())
+                  .nickname(member.getNickname())
+                  .profileImageUrl(member.getProfileImageUrl())
+                  .phone(member.getPhone())
+                  .build())
+          .status(member.getStatus())
+          .role(member.getRole())
+          .createdAt(member.getCreatedAt())
+          .updatedAt(member.getUpdatedAt())
+          .build();
+    }
+  }
+}
+
