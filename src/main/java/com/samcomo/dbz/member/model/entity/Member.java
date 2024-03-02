ccd78c1c242c877eded5b6bf2e47a140c385a53b
JGoo99 <zzgg3040@naver.com>
2024-03-03 03:49:45 +0900

    [#17]refactor: baseEntity 설정 변경
    - abstract 클래스로 변경
    - 어노테이션 변경(이슈참고)

--- src/main/java/com/samcomo/dbz/member/model/entity/Member.java
+++ src/main/java/com/samcomo/dbz/member/model/entity/Member.java
@@ -1,27 +1,25 @@
 package com.samcomo.dbz.member.model.entity;
 
 import com.samcomo.dbz.global.entity.BaseEntity;
+import com.samcomo.dbz.member.model.constants.MemberRole;
 import com.samcomo.dbz.member.model.constants.MemberStatus;
-import com.samcomo.dbz.member.model.constants.UserRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Entity
-@Builder
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -32,15 +30,35 @@ public class Member extends BaseEntity {
 
   private String nickname;
 
-  private String imageUrl;
+  private String profileImageUrl;
 
   private String phone;
 
   @Enumerated(EnumType.STRING)
-  private UserRole userRole;
+  private MemberRole role;
 
   @Enumerated(EnumType.STRING)
   private MemberStatus status;
 
   private String fcmKey;
+
+  @Builder
+  public Member(Long id, String email, String password, String nickname, String phone,
+      String profileImageUrl, MemberRole role, MemberStatus status, String fcmKey) {
+
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.nickname = nickname;
+    this.phone = phone;
+    this.profileImageUrl = profileImageUrl;
+    this.role = role;
+    this.status = status;
+    this.fcmKey = fcmKey;
+  }
+
+  public void encodePassword(PasswordEncoder passwordEncoder, String rawPassword) {
+
+    this.password = passwordEncoder.encode(rawPassword);
+  }
 }
