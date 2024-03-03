2e0379bf9467edf028b6953173d95c3fbc0e7b61
구진 <126454114+JGoo99@users.noreply.github.com>
2024-03-03 19:01:53 +0900

    [#17]refactor: baseEntity 설정 변경 (#19)
    - abstract 클래스로 변경
    - 어노테이션 변경(이슈참고)

--- src/main/java/com/samcomo/dbz/DbzApplication.java
+++ src/main/java/com/samcomo/dbz/DbzApplication.java
@@ -2,7 +2,11 @@ package com.samcomo.dbz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
+@EnableJpaAuditing
 @SpringBootApplication
 public class DbzApplication {
 
--- src/main/java/com/samcomo/dbz/global/entity/BaseEntity.java
+++ src/main/java/com/samcomo/dbz/global/entity/BaseEntity.java
@@ -1,21 +1,23 @@
 package com.samcomo.dbz.global.entity;
 
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
 @Getter
+@MappedSuperclass
+@NoArgsConstructor
 @EntityListeners(value = {AuditingEntityListener.class})
-public class BaseEntity {
+public abstract class BaseEntity {
 
   @CreatedDate
   private LocalDateTime createdAt;
 
   @LastModifiedDate
   private LocalDateTime updatedAt;
-
 }
