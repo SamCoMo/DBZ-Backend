836ad83ff7d2ebba1f5f07dfafcf842bbc75ef5e
JGoo99 <zzgg3040@naver.com>
2024-03-06 00:22:50 +0900

    feat: jwt 인증/인가 구현

---
+++ src/main/java/com/samcomo/dbz/member/service/impl/MemberDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.samcomo.dbz.member.service.impl;
+
+import com.samcomo.dbz.global.exception.ErrorCode;
+import com.samcomo.dbz.member.exception.MemberException;
+import com.samcomo.dbz.member.model.dto.MemberDetails;
+import com.samcomo.dbz.member.model.entity.Member;
+import com.samcomo.dbz.member.model.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsServiceImpl implements UserDetailsService {
+
+  private final MemberRepository memberRepository;
+
+  @Override
+  public MemberDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+    Member member = memberRepository.findByEmail(email).orElseThrow(() ->
+        new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+
+    return new MemberDetails(member);
+  }
+}
