e375dfa653e6353a2e8a765e4deb3cbcec41d184
JGoo99 <zzgg3040@naver.com>
2024-03-13 16:40:19 +0900

    refactor: dto & entity 수정

--- src/main/java/com/samcomo/dbz/report/model/dto/CustomPageable.java
+++ src/main/java/com/samcomo/dbz/report/model/dto/CustomPageable.java
@@ -1,15 +1,20 @@
 package com.samcomo.dbz.report.model.dto;
 
-import lombok.AllArgsConstructor;
+import static lombok.AccessLevel.PROTECTED;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
 public class CustomPageable {
+
+  public CustomPageable(int pageNumber, int pageSize) {
+
+    this.pageNumber = pageNumber;
+    this.pageSize = pageSize;
+  }
 
   private int pageNumber;
   private int pageSize;
-
 }
