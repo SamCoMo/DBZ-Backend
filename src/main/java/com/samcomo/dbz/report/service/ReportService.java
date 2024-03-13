514a8af5e70a108337a89cd3d9345e2aee1337ef
JGoo99 <zzgg3040@naver.com>
2024-03-13 16:41:24 +0900

    refactor: 로그인 정보 가져오기 수정
    - 이메일 정보가 아닌 member 정보로 게시글 DB 조회로 변경

--- src/main/java/com/samcomo/dbz/report/service/ReportService.java
+++ src/main/java/com/samcomo/dbz/report/service/ReportService.java
@@ -1,26 +1,29 @@
 package com.samcomo.dbz.report.service;
 
+import com.samcomo.dbz.member.model.entity.Member;
 import com.samcomo.dbz.report.model.dto.CustomSlice;
 import com.samcomo.dbz.report.model.dto.ReportDto;
-import com.samcomo.dbz.report.model.dto.ReportList;
 import com.samcomo.dbz.report.model.dto.ReportStateDto.Response;
+import com.samcomo.dbz.report.model.dto.ReportSummaryDto;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ReportService {
 
-  ReportDto.Response uploadReport(String email, ReportDto.Form reportForm, List<MultipartFile> imageList);
+  ReportDto.Response uploadReport(Member member, ReportDto.Form reportForm,
+      List<MultipartFile> imageList);
 
-  ReportDto.Response getReport(long reportId, String email);
-  CustomSlice<ReportList> getReportList(
+  ReportDto.Response getReport(long reportId, Member member);
+  CustomSlice<ReportSummaryDto> getReportList(
       double lastLatitude, double lastLongitude, double curLatitude, double curLongitude, boolean showsInProcessOnly, Pageable pageable);
 
-  ReportDto.Response updateReport(long reportId, ReportDto.Form reportForm, List<MultipartFile> imageList, String email);
+  ReportDto.Response updateReport(long reportId, ReportDto.Form reportForm,
+      List<MultipartFile> imageList, Member member);
 
-  Response deleteReport(String email, long reportId);
+  Response deleteReport(Member member, long reportId);
 
-  Response changeStatusToFound(String email, long reportId);
+  Response changeStatusToFound(Member member, long reportId);
 
-  CustomSlice<ReportList> searchReport(String object, boolean showsInProgressOnly, Pageable pageable);
+  CustomSlice<ReportSummaryDto> searchReport(String object, boolean showsInProgressOnly, Pageable pageable);
 }
