5d38a5d3bd87c82578e021e900403fc947fc7027
JGoo99 <zzgg3040@naver.com>
2024-03-16 01:33:03 +0900

    refactor: 컨벤션 & 공백 & 로직 정리

--- src/main/java/com/samcomo/dbz/pin/service/PinService.java
+++ src/main/java/com/samcomo/dbz/pin/service/PinService.java
@@ -1,21 +1,17 @@
 package com.samcomo.dbz.pin.service;
 
-import com.samcomo.dbz.pin.dto.CreatePinDto.Request;
-import com.samcomo.dbz.pin.dto.CreatePinDto.Response;
-import com.samcomo.dbz.pin.dto.UpdatePinAddressDto;
-import com.samcomo.dbz.pin.dto.UpdatePinDataDto;
+import com.samcomo.dbz.pin.dto.RegisterPinDto.Request;
+import com.samcomo.dbz.pin.dto.RegisterPinDto.Response;
+import com.samcomo.dbz.pin.dto.UpdatePinDto;
 
 public interface PinService {
 
   // 핀 생성
-  Response createPin(String memberEmail,Long reportId, Request request);
-
-  // 핀 주소 업데이트
-  UpdatePinAddressDto.Response updatePinAddress(String memberEmail, Long pinId, UpdatePinAddressDto.Request request);
+  Response createPin(Long memberId, Long reportId, Request request);
 
   // 핀 업데이트 (주소는 바꿀수 없습니다.)
-  UpdatePinDataDto.Response updatePinData(String memberEmail, Long pinId, UpdatePinDataDto.Response updatePinResponseDto);
+  UpdatePinDto.Response updatePin(Long memberId, Long pinId, UpdatePinDto.Response updatePinResponseDto);
 
   // 핀 삭제
-  void deletePin(String memberEmail, Long pinId);
+  void deletePin(Long memberId, Long pinId);
 }
