cda214835e805a84f65a1be8e4cc7c3965b4c2f3
JGoo99 <zzgg3040@naver.com>
2024-03-21 03:06:30 +0900

    refactor: 로그인 필터 메서드 분리

--- src/main/java/com/samcomo/dbz/member/jwt/filter/CustomLoginFilter.java
+++ src/main/java/com/samcomo/dbz/member/jwt/filter/CustomLoginFilter.java
@@ -3,19 +3,18 @@ package com.samcomo.dbz.member.jwt.filter;
 import static com.samcomo.dbz.member.model.constants.TokenType.ACCESS_TOKEN;
 import static com.samcomo.dbz.member.model.constants.TokenType.REFRESH_TOKEN;
 
+import com.samcomo.dbz.global.exception.ErrorCode;
+import com.samcomo.dbz.member.exception.MemberException;
 import com.samcomo.dbz.member.jwt.JwtUtil;
 import com.samcomo.dbz.member.model.dto.MemberDetails;
 import com.samcomo.dbz.member.model.entity.RefreshToken;
 import com.samcomo.dbz.member.model.repository.RefreshTokenRepository;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Date;
 import java.util.Iterator;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -24,7 +23,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+public class CustomLoginFilter extends AbstractAuthenticationProcessingFilter {
 
   private final JwtUtil jwtUtil;
   private final RefreshTokenRepository refreshTokenRepository;
@@ -35,9 +34,8 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
   private static final String EMAIL_KEY = "email";
   private static final String PASSWORD_KEY = "password";
 
-  public LoginFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil,
-      RefreshTokenRepository refreshTokenRepository) {
-
+  public CustomLoginFilter(
+      AuthenticationManager authenticationManager, JwtUtil jwtUtil, RefreshTokenRepository refreshTokenRepository) {
     super(LOGIN_REQUEST_MATCHER, authenticationManager);
     this.jwtUtil = jwtUtil;
     this.refreshTokenRepository = refreshTokenRepository;
@@ -45,8 +43,7 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 
   @Override
   public Authentication attemptAuthentication(
-      HttpServletRequest request, HttpServletResponse response)
-      throws AuthenticationException, IOException, ServletException {
+      HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
     String email = obtainEmail(request);
     email = (email != null) ? email.trim() : "";
@@ -71,47 +68,48 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
   // 로그인 성공 시 JWT 발급
   @Override
   protected void successfulAuthentication(HttpServletRequest request,
-      HttpServletResponse response, FilterChain chain, Authentication authResult)
-      throws IOException, ServletException {
+      HttpServletResponse response, FilterChain chain, Authentication authResult) {
 
-    MemberDetails memberDetails = (MemberDetails) authResult.getPrincipal();
-    String memberId = String.valueOf(memberDetails.getId());
-
-    Iterator<? extends GrantedAuthority> iterator = authResult.getAuthorities().iterator();
-    GrantedAuthority auth = iterator.next();
-    String role = auth.getAuthority();
+    String memberId = getMemberId(authResult);
+    String role = getMemberRole(authResult);
 
     String accessToken = jwtUtil.createToken(ACCESS_TOKEN, memberId, role);
     String refreshToken = jwtUtil.createToken(REFRESH_TOKEN, memberId, role);
 
-    addRefreshToken(memberId, refreshToken);
+    saveRefreshTokenToDataBase(memberId, refreshToken);
 
     response.setHeader(ACCESS_TOKEN.getKey(), accessToken);
-    response.addCookie(createCookie(REFRESH_TOKEN.getKey(), refreshToken));
-    response.setStatus(HttpStatus.OK.value());
+    response.addCookie(createCookie(refreshToken));
+    response.setStatus(HttpServletResponse.SC_OK);
+  }
+
+  private String getMemberId(Authentication authResult) {
+    MemberDetails memberDetails = (MemberDetails) authResult.getPrincipal();
+    return String.valueOf(memberDetails.getId());
+  }
+
+  private String getMemberRole(Authentication authResult) {
+    Iterator<? extends GrantedAuthority> iterator = authResult.getAuthorities().iterator();
+    GrantedAuthority auth = iterator.next();
+    return auth.getAuthority();
   }
 
   // 로그인 실패 응답 간이 구현
   @Override
   protected void unsuccessfulAuthentication(HttpServletRequest request,
-      HttpServletResponse response, AuthenticationException failed)
-      throws IOException, ServletException {
-
-    response.setStatus(401);
+      HttpServletResponse response, AuthenticationException failed) {
+    throw new MemberException(ErrorCode.AUTHENTICATION_FAILED);
   }
 
-  private Cookie createCookie(String key, String value) {
-
-    Cookie cookie = new Cookie(key, value);
+  private Cookie createCookie(String refreshToken) {
+    Cookie cookie = new Cookie(REFRESH_TOKEN.getKey(), refreshToken);
     cookie.setMaxAge(24 * 60 * 60);
-    // cookie.setSecure(true); csrf 공격 방지 (https 가 적용되지 않은 이미지 경로 접근 방지)
+    // cookie.setSecure(true); csrf 공격 방지
     cookie.setHttpOnly(true); // xss 공격 방지 (js 접근 불가)
-
     return cookie;
   }
 
-  private void addRefreshToken(String memberId, String refreshToken) {
-
+  private void saveRefreshTokenToDataBase(String memberId, String refreshToken) {
     Date expiration = new Date(System.currentTimeMillis() + REFRESH_TOKEN.getExpiredMs());
 
     refreshTokenRepository.save(RefreshToken.builder()
