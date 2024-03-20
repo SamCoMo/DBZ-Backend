0ac95d30f6df0f8668bd298ce048f8f35a3e91e9
JGoo99 <zzgg3040@naver.com>
2024-03-21 03:03:53 +0900

    bug: 필터 동작 에러 해결
    - boolean 값 반대로 설정된 오류 수정

--- src/main/java/com/samcomo/dbz/member/jwt/filter/RefreshTokenFilter.java
+++ src/main/java/com/samcomo/dbz/member/jwt/filter/RefreshTokenFilter.java
@@ -47,18 +47,22 @@ public class RefreshTokenFilter implements JwtFilter {
 
   public void validateRefreshToken(String oldRefreshToken) {
 
-    if (isNull(oldRefreshToken) || isTokenTypeCorrect(oldRefreshToken)) {
+    if (isNull(oldRefreshToken) || !isTokenTypeCorrect(oldRefreshToken)) {
       throw new MemberException(INVALID_REFRESH_TOKEN);
     }
 
     // 탈취한 refresh1 로 새로운 refresh2 가 생성된 경우 기존 유저의 refresh1 은 DB 에 존재하지 않는다.
-    if (isTokenInDataBase(oldRefreshToken)) {
+    if (!isTokenInDataBase(oldRefreshToken)) {
       // 탈취 가능성을 고려하여 토큰을 전부 삭제한 후 재로그인을 요청한다.
       deleteAllRefreshTokenFromDataBase(oldRefreshToken);
       throw new MemberException(REFRESH_TOKEN_NOT_FOUND);
     }
 
-    checkExpiration(oldRefreshToken);
+    try {
+      checkExpiration(oldRefreshToken);
+    } catch (ExpiredJwtException e) {
+      throw new MemberException(REFRESH_TOKEN_EXPIRED);
+    }
   }
 
   private String getNewToken(TokenType tokenType, String oldRefreshToken) {
@@ -84,7 +88,7 @@ public class RefreshTokenFilter implements JwtFilter {
   private Cookie createCookie(String newRefreshToken) {
     Cookie cookie = new Cookie(REFRESH_TOKEN.getKey(), newRefreshToken);
     cookie.setMaxAge(24 * 60 * 60);
-    // cookie.setSecure(true); csrf 공격 방지 (https 가 적용되지 않은 이미지 경로 접근 방지)
+    // cookie.setSecure(true); // csrf 공격 방지
     cookie.setHttpOnly(true); // xss 공격 방지 (js 접근 불가)
     return cookie;
   }
@@ -100,12 +104,8 @@ public class RefreshTokenFilter implements JwtFilter {
   }
 
   @Override
-  public void checkExpiration(String token) {
-    try {
-      jwtUtil.isExpired(token);
-    } catch (ExpiredJwtException e) {
-      throw new MemberException(REFRESH_TOKEN_EXPIRED);
-    }
+  public void checkExpiration(String token) throws ExpiredJwtException {
+    jwtUtil.isExpired(token);
   }
 
   @Override
@@ -113,7 +113,7 @@ public class RefreshTokenFilter implements JwtFilter {
     return jwtUtil.getTokenType(token).equals(REFRESH_TOKEN.getKey());
   }
 
-  private boolean isTokenInDataBase(String token) {
+  public boolean isTokenInDataBase(String token) {
     Long memberId = getMemberId(token);
     return refreshTokenRepository.existsByRefreshTokenAndMemberId(token, memberId);
   }
