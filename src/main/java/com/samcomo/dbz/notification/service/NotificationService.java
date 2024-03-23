691820f2a6ccfaab2f3dcf79f4f97ce7e98126bb
UNA <lemonlee030@gmail.com>
2024-03-23 17:01:57 +0900

    feature: noti List 조회 api

--- src/main/java/com/samcomo/dbz/notification/service/NotificationService.java
+++ src/main/java/com/samcomo/dbz/notification/service/NotificationService.java
@@ -1,14 +1,18 @@
 package com.samcomo.dbz.notification.service;
 
+import com.samcomo.dbz.notification.model.dto.NotificationDto;
 import com.samcomo.dbz.report.model.dto.ReportDto;
 import java.io.IOException;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface FcmService {
+public interface NotificationService {
   void sendPinNotification(Long memberId) throws IOException;
 
   void sendReportNotification(Long memberId, ReportDto.Form reportForm);
+
+  List<NotificationDto> getNotificationList(Long memberId);
 
 //  void sendChatNotification();
 }
