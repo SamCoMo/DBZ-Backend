b40f582ee54579bddb6d52e43b1f3940ce95bc8b
구진 <126454114+JGoo99@users.noreply.github.com>
2024-03-25 23:30:53 +0900

    test 완료 (#105)

--- src/main/java/com/samcomo/dbz/member/model/constants/TokenType.java
+++ src/main/java/com/samcomo/dbz/member/model/constants/TokenType.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TokenType {
 
-  ACCESS_TOKEN("Access-Token", 6000L * 5),  // 30초
-  REFRESH_TOKEN("Refresh-Token", 6000L * 10);  // 1분
+  ACCESS_TOKEN("Access-Token", 6000L * 10 * 10),  // 10분
+  REFRESH_TOKEN("Refresh-Token", 6000L * 10 * 60 * 24);  // 24시간
 
   private final String key;
   private final Long expiredMs;
