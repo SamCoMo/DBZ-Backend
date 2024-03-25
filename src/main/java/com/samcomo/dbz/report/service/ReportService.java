58f4ff3982c6c075d544a2d5515036a9b9827fc7
jujaeyeong <jjy5230@gmail.com>
2024-03-25 13:28:33 +0900

    deploy : 배포 V2.7

--- src/main/java/com/samcomo/dbz/report/service/ReportService.java
+++ src/main/java/com/samcomo/dbz/report/service/ReportService.java
@@ -2,23 +2,21 @@ package com.samcomo.dbz.report.service;
 
 import com.samcomo.dbz.report.model.dto.CustomSlice;
 import com.samcomo.dbz.report.model.dto.ReportDto;
-import com.samcomo.dbz.report.model.dto.ReportStateDto.Response;
 import com.samcomo.dbz.report.model.dto.ReportSearchSummaryDto;
+import com.samcomo.dbz.report.model.dto.ReportStateDto.Response;
 import com.samcomo.dbz.report.model.dto.ReportSummaryDto;
-import java.util.List;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface ReportService {
 
-  ReportDto.Response uploadReport(long memberId, ReportDto.Form reportForm, List<MultipartFile> imageList);
+  ReportDto.Response uploadReport(long memberId, ReportDto.Form reportForm);
 
   ReportDto.Response getReport(long reportId, long memberId);
 
   CustomSlice<ReportSummaryDto> getReportList(
       double lastLatitude, double lastLongitude, double curLatitude, double curLongitude, boolean showsInProcessOnly, Pageable pageable);
 
-  ReportDto.Response updateReport(long reportId, long memberId, ReportDto.Form reportForm, List<MultipartFile> imageList);
+  ReportDto.Response updateReport(long reportId, long memberId, ReportDto.Form reportForm);
 
   Response deleteReport(long reportId, long memberId);
 
