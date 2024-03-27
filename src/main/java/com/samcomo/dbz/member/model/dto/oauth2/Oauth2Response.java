407c1d53ab96f5056744cab9f45f002e744c4866
구진 <126454114+JGoo99@users.noreply.github.com>
2024-03-28 00:02:41 +0900

    update main (#116)
    * refactor: move pakage (#114)
    
    * [Feature/member] 구글 소셜 로그인 (#115)
    
    * refactor: move pakage
    
    * feat: 구글 소셜로그인
    
    * refactor: 디렉토리 위치 수정

---
+++ src/main/java/com/samcomo/dbz/member/model/dto/oauth2/Oauth2Response.java
@@ -0,0 +1,11 @@
+package com.samcomo.dbz.member.model.dto.oauth2;
+
+import com.samcomo.dbz.member.model.constants.LoginType;
+
+public interface Oauth2Response {
+
+  LoginType getLoginType();
+  String getEmail();
+  String getNickname();
+  String getProfileImageUrl();
+}
