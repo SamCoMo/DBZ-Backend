ad2e7e6cda60706e6eb00066d98f302fd93c0ffe
JGoo99 <zzgg3040@naver.com>
2024-04-02 11:26:11 +0900

    로그인 시간 임시로 늘림

--- src/main/java/com/samcomo/dbz/member/model/constants/TokenType.java
+++ src/main/java/com/samcomo/dbz/member/model/constants/TokenType.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TokenType {
 
-  ACCESS_TOKEN("Access-Token", 6000L * 10 * 10),  // 10분
-  REFRESH_TOKEN("Refresh-Token", 6000L * 10 * 60 * 24);  // 24시간
+  ACCESS_TOKEN("Access-Token", 6000L * 10 * 1000000),  // 10분 -> 임시로 늘림
+  REFRESH_TOKEN("Refresh-Token", 6000L * 10 * 60 * 2400000);  // 24시간 -> 임시로 늘림
 
   private final String key;
   private final Long expiredMs;
